b7540e41d964442abe26261fe037631db2ee771f
Dan6erbond <moravrav@gmail.com>
2019-05-13 21:07:30 +0200

    animations + block exit in lang builder

--- Builder/src/main/java/Builder.java
+++ Builder/src/main/java/Builder.java
@@ -1,9 +1,14 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -15,11 +20,23 @@ public class Builder extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        Font.loadFont(Builder.class.getResource("/fonts/Roboto-Regular.ttf").toExternalForm(), 12);
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Home.fxml"));
+
         Parent root = loader.load();
+        root.setOpacity(0);
+        root.setStyle("-accent-color: white;");
+
         Scene scene = new Scene(root);
-        Font.loadFont(Builder.class.getResource("/fonts/Roboto-Regular.ttf").toExternalForm(), 10);
         primaryStage.setScene(scene);
+
+        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setResizable(false);
         primaryStage.show();
+
+        Timeline timeline = new Timeline();
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(0.5), new KeyValue(root.opacityProperty(), 1)));
+        timeline.play();
     }
 }
--- Builder/src/main/java/HomeController.java
+++ Builder/src/main/java/HomeController.java
@@ -1,3 +1,6 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -5,6 +8,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
@@ -28,8 +33,35 @@ public class HomeController implements Initializable {
 
     private void openScene(Stage stage, String fxml) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+
         Parent root = loader.load();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+        root.setOpacity(0);
+
+        stage.setScene(new Scene(root));
+
+        stage.setOnHidden(event -> {
+            try{
+                FXMLLoader l = new FXMLLoader(getClass().getResource("/fxml/Home.fxml"));
+
+                Parent r = l.load();
+                r.setOpacity(0);
+
+                Stage s = new Stage(StageStyle.UTILITY);
+                s.setResizable(false);
+                s.setScene(new Scene(r));
+
+                Timeline timeline = new Timeline();
+                timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(0.5), new KeyValue(r.opacityProperty(), 1)));
+                timeline.play();
+
+                s.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Timeline timeline = new Timeline();
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(0.5), new KeyValue(root.opacityProperty(), 1)));
+        timeline.play();
     }
 }
